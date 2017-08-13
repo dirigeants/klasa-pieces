c215edb44f5e9c96eb09535f658afd3bbe9681cc
kyraNET <kyradiscord@gmail.com>
2017-08-13 21:24:23 +0200

    Renamed monitor

--- monitors/invitedetection.js
+++ monitors/invitedetection.js
@@ -4,7 +4,7 @@ module.exports = class extends Monitor {
 
     constructor(...args) {
         super(...args, {
-            name: 'antiinvite',
+            name: 'invitedetection',
             enabled: true,
             ignoreSelf: true
         });
