1cda96722f3f92c76a2d9af352468a78a9c0a9be
kyraNET <kyranet@users.noreply.github.com>
2017-08-18 13:13:30 +0200

    Commands... finished!

---
+++ commands/System/exec.js
@@ -0,0 +1,27 @@
+const { Command, util } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			permLevel: 10,
+
+			description: 'Execute commands in the terminal, use with EXTREME CAUTION.',
+			usage: '<expression:str>'
+		});
+
+		this.pieces = {
+			type: 'commands',
+			requiredModules: []
+		};
+	}
+
+	async run(msg, [input]) {
+		const result = await util.exec(input).catch((err) => { throw err; });
+
+		const output = result.stdout ? `**\`OUTPUT\`**${util.codeBlock('sh', result.stdout)}` : '';
+		const outerr = result.stderr ? `**\`ERROR\`**${util.codeBlock('sh', result.stderr)}` : '';
+		return msg.send([output, outerr].join('\n'));
+	}
+
+};
