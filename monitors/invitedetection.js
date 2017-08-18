a77c9255bfc8756ef154601968b6980a47bca991
kyraNET <kyranet@users.noreply.github.com>
2017-08-18 19:48:28 +0200

    Added inhibitors and monitors

--- monitors/invitedetection.js
+++ monitors/invitedetection.js
@@ -2,25 +2,26 @@ const { Monitor } = require('klasa');
 
 module.exports = class extends Monitor {
 
-    constructor(...args) {
-        super(...args, {
-            name: 'invitedetection',
-            ignoreSelf: true
-        });
-    }
+	constructor(...args) {
+		super(...args, {
+			name: 'invitedetection',
+			enabled: true,
+			ignoreSelf: true
+		});
+	}
 
-    run(msg) {
-        if (msg.channel.type !== 'text' || msg.guild.settings.antiinvite !== true) return null;
-        if (msg.hasAtleastPermissionLevel(6)) return null;
-        if (!/(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(msg.content)) return null;
-        return msg.delete()
-            .catch(err => this.client.emit('log', err, 'error'));
-    }
+	run(msg) {
+		if (msg.channel.type !== 'text' || msg.guild.settings.antiinvite !== true) return null;
+		if (msg.hasAtleastPermissionLevel(6)) return null;
+		if (!/(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(msg.content)) return null;
+		return msg.delete()
+			.catch(err => this.client.emit('log', err, 'error'));
+	}
 
-    async init() {
-        if (!this.client.settings.guilds.schema.antiinvite) {
-            await this.client.settings.guilds.add('antiinvite', { type: 'Boolean', default: false });
-        }
-    }
+	async init() {
+		if (!this.client.settings.guilds.schema.antiinvite) {
+			await this.client.settings.guilds.add('antiinvite', { type: 'Boolean', default: false });
+		}
+	}
 
 };
