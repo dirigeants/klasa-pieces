af6a72e6e68bc75a9504e2730a0e712fa0d3f3e1
MrJacz <jacknagle22@gmail.com>
2017-10-30 06:49:13 +1100

    Update username.js

--- extendables/username.js
+++ extendables/username.js
@@ -1,14 +1,13 @@
 const { Extendable } = require('klasa');
 const { GuildMember, User, Message } = require('discord.js');
 const userRegex = new RegExp(/^(?:<@!?)?(\\d{17,19})>?$/);
-module.exports = class extends Extendable {
+ module.exports = class extends Extendable {
 
 	constructor(...args) {
 		super(...args, ['ArgResolver'], { klasa: true });
 	}
 
 	async extend(arg, currentUsage, possible, repeat, msg) {
-		if (arg instanceof User) return arg;
 		if (arg instanceof GuildMember) return arg.user;
 		if (arg instanceof Message) return arg.author;
 		const matches = userRegex.exec(arg);
@@ -24,7 +23,6 @@ module.exports = class extends Extendable {
 		if (users.length === 1) return users[0];
 		const exactUsers = users.filter(userFilterExact(search));
 		if (exactUsers.length === 1) return exactUsers[0];
-		if (exactUsers.length > 0) users = exactUsers;
 		if (currentUsage.type === 'optional' && !repeat) return null;
 		if (users.length > 15) throw 'Multiple users found. Please be more specific.';
 		throw `${currentUsage.possibles[possible].name} Must be a vaild mention, id or username`;
@@ -40,6 +38,6 @@ function userFilterExact(search) {
 
 function userFilterInexact(search) {
 	return user => user.username.toLowerCase().includes(search) ||
-		`${user.username.toLowerCase()}#${user.discriminator}`.includes(search);
+		`${user.username.toLowerCase()}#${user.discriminator}`.indexOf(search);
 }
 
