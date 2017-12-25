1e26a97523caaa76a0af7f45ef458265e570df46
kyraNET <kyradiscord@gmail.com>
2017-12-25 20:55:01 +0100

    Class/Object to TypeScript (WIP)

--- commands/System/type.js
+++ commands/System/type.js
@@ -20,47 +20,72 @@ module.exports = class extends Command {
 		const result = this.eval(msg, code);
 		const evaled = stopwatch.friendlyDuration;
 		stopwatch.restart();
-		const type = this.util.isThenable(result) ?
-			`${this.util.getTypeName(result)}<${this.getDeepTypes(await result.catch(error => error))}>` :
-			this.getDeepTypes(result);
+		const type = this.getKeyTypeNames(result);
 		const parsed = stopwatch.friendlyDuration;
 		stopwatch.stop();
 
-		return msg.sendMessage(`\`⏱ EXEC: ${evaled} | PARSE: ${parsed}\`  The type is: ${this.util.codeBlock('ts', type)}`);
+		return msg.sendMessage(`\`⏱ EXEC: ${evaled} | PARSE: ${parsed}\`  The type is: ${this.util.codeBlock('ts',
+			typeof type === 'string' ? type : this.drawTypeDescription('Complex', result, type))}`);
 	}
 
-	getDeepTypes(result) {
-		const basic = this.util.getTypeName(result);
-		switch (basic) {
-			case 'Map':
-			case 'Collection':	{
-				const typeKeys = new Set(),
-					typeValues = new Set();
-				for (const [key, value] of result) {
-					const typeKey = this.getDeepTypes(key);
-					if (!typeKeys.has(typeKey)) typeKeys.add(typeKey);
-					const typeValue = this.getDeepTypes(value);
-					if (!typeValues.has(typeValue)) typeValues.add(typeValue);
-				}
-				const typeK = typeKeys.size === 0 || typeKeys.has('Object') ? 'any' : Array.from(typeKeys).sort().join(' | ');
-				const typeV = typeValues.size === 0 || typeValues.has('Object') ? 'any' : Array.from(typeValues).sort().join(' | ');
+	getKeyTypeNames(result) {
+		if (!this.util.isObject(result)) return this.util.getDeepTypeName(result);
 
-				return `${basic}<${typeK}, ${typeV}>`;
-			}
-			case 'Set':
-			case 'Array': {
-				const types = new Set();
-				for (const value of result) {
-					const type = this.getDeepTypes(value);
-					if (!types.has(type)) types.add(type);
-				}
-				const typeV = types.size === 0 || types.has('Object') ? 'any' : Array.from(types).sort().join(' | ');
-
-				return `${basic}<${typeV}>`;
-			}
-			default:
-				return basic;
+		const object = {};
+		const keys = Object.getOwnPropertyNames(result);
+		for (const key of keys) {
+			object[key] = {
+				name: key,
+				descriptor: Object.getOwnPropertyDescriptor(result, key),
+				isPublic: !key.startsWith('_')
+			};
 		}
+		const proto = Object.getPrototypeOf(result);
+		if (result === proto) return object;
+
+		if (proto && proto.constructor && proto.constructor.name === 'Object') return object;
+		const protoKeys = proto ? Object.getOwnPropertyNames(proto) : [];
+		if (protoKeys.length > 0) {
+			for (const key of protoKeys) {
+				object[key] = {
+					name: key,
+					descriptor: Object.getOwnPropertyDescriptor(proto, key),
+					isPublic: !key.startsWith('_')
+				};
+			}
+		}
+
+		return object;
+	}
+
+	drawTypeDescription(name, result, parsedObject) {
+		const proto = Object.getPrototypeOf(result);
+		const className = proto && proto.constructor && proto.constructor.name !== 'Object' ? proto.constructor.name : null;
+		const output = [];
+		for (const key of Object.keys(parsedObject)) {
+			const { descriptor, isPublic } = parsedObject[key];
+			if (descriptor.set) output.push(this.drawType(isPublic, 'set', key, descriptor.set));
+			if (descriptor.get) output.push(this.drawType(isPublic, 'get', key, descriptor.get));
+			if (descriptor.value) {
+				if (typeof descriptor.value === 'function') output.push(this.drawType(isPublic, 'method', key, descriptor.value));
+				else output.push(this.drawType(isPublic, null, key, descriptor.value));
+			}
+		}
+		output.sort((a, b) => b.localeCompare(a));
+		return (className ? `export class ${className} {\n` : `export type ${name} = {\n`) + output.join('\n') + (className ? '\n}' : '\n};');
+	}
+
+	drawType(accessorLevel, type, name, value) {
+		if (type === 'set' || type === 'method') return `\t${accessorLevel ? 'public' : 'private'} ${type === 'set' ? 'set ' : ''}${name}${this.getFunctionParams(value)}: any;`;
+		if (type === 'get') return `\t${accessorLevel ? 'public' : 'private'} readonly ${name}: any;`;
+		return `\t${accessorLevel ? 'public' : 'private'} ${name}: ${this.util.getDeepTypeName(value)};`;
+	}
+
+	getFunctionParams(fn) {
+		if (fn.length === 0) return '()';
+		const fnStr = fn.toString();
+		if (/\([^)]+\)/.test(fnStr)) return /\([^)]+\)/.exec(fnStr)[0];
+		return '()';
 	}
 
 	eval(msg, code) {
