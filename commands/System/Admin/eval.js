4dc625abd1f12cdc3e92d0e7a2c7f05d9edd22ea
kyraNET <kyradiscord@gmail.com>
2018-01-16 15:28:23 +0100

    Added the i18n keys for the (Mini)Lord of the Evals

--- commands/System/Admin/eval.js
+++ commands/System/Admin/eval.js
@@ -146,3 +146,12 @@ module.exports = class extends Command {
 	The --showHidden flag will enable the showHidden option in util.inspect.
 	If the output is too large, it'll send the output as a file, or in the console if the bot does not have the ATTACH_FILES permission.
  */
+
+/**
+	COMMAND_EVAL_TIMEOUT: (seconds) => `TIMEOUT: Took longer than ${seconds} seconds.`,
+	COMMAND_EVAL_ERROR: (time, output, type) => `**Error**:${output}\n**Type**:${type}\n${time}`,
+	COMMAND_EVAL_OUTPUT: (time, output, type) => `**Output**:${output}\n**Type**:${type}\n${time}`,
+	COMMAND_EVAL_OUTPUT_CONSOLE: (time, type) => `Sent the result to console.\n**Type**:${type}\n${time}`,
+	COMMAND_EVAL_OUTPUT_FILE: (time, type) => `Sent the result as a file.\n**Type**:${type}\n${time}`,
+	COMMAND_EVAL_OUTPUT_HASTEBIN: (time, url, type) => `Sent the result to hastebin: ${url}\n**Type**:${type}\n${time}\n`
+*/
