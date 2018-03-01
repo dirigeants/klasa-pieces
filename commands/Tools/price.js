e59fe73a5abfd5b138d1635faf1fe93bcb8a7cd5
gchippendale <30398469+gchippendale@users.noreply.github.com>
2018-03-02 03:33:47 +1100

    Fix
    - Proper UsageString validation
    - Added 'amount'

--- commands/Tools/price.js
+++ commands/Tools/price.js
@@ -1,26 +1,23 @@
-const { Command } = require('klasa');
-const snekfetch = require('snekfetch');
+const { Command } = require("klasa");
+const snekfetch = require("snekfetch");
 
 module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Compares the value of a currency (crypto, fiat) with another.",
+      usage: "<coin:str{1,3}> <currency:str{1,3}> [amount:int{1}]",
+      usageDelim: " "
+    });
+  }
 
-	constructor(...args) {
-		super(...args, {
-			description: 'Returns the current price of a Cryptocurrency Coin',
-			usage: '<coin:str> <currency:str>',
-			usageDelim: ' '
-		});
-	}
+  async run(msg, [coin, currency, amount = 1]) {
+    const c1 = coin.toUpperCase();
+    const c2 = currency.toUpperCase();
 
-	async run(msg, [coin, currency]) {
-		const c1 = coin.toUpperCase();
-		const c2 = currency.toUpperCase();
-
-		const res = await snekfetch.get(`https://min-api.cryptocompare.com/data/price?fsym=${c1}&tsyms=${c2}`)
-			.catch(() => { throw 'There was an error, please make sure you specified an appropriate coin and currency.'; });
-
-		return msg.send(res.body[c2] ?
-			`Current ${c1} price is ${res.body[c2]} ${c2}` :
-			'There was an error, please make sure you specified an appropriate coin and currency.');
-	}
-
+    const res = await snekfetch.get(`https://min-api.cryptocompare.com/data/price?fsym=${c1}&tsyms=${c2}`).catch(() => {
+      throw "There was an error, please make sure you specified an appropriate coin and currency.";
+    });
+    if (!res.body[c2]) return msg.send("There was an error, please make sure you specified an appropriate coin and currency.");
+    return msg.send(`Current price of ${amount} ${c1} is ${res.body[c2].toLocaleString()} ${c2}`);
+  }
 };
