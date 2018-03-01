54f4c2e5b76397487368138a07cfcb79aca6d0ed
gc-au <30398469+gc-au@users.noreply.github.com>
2018-03-02 04:01:07 +1100

    Create prefix.js
    This is a shortcut for setting prefix with `+conf set prefix`. more user-friendly and will show up in +help.

---
+++ commands/Configuration/prefix.js
@@ -0,0 +1,18 @@
+const { Command } = require("klasa");
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      permLevel: 6,
+      runIn: ["text"],
+      cooldown: 5,
+      description: "Change the command prefix the bot uses in your server.",
+      usage: "<prefix:str{1,4}>"
+    });
+  }
+
+  async run(msg, [prefix]) {
+    await msg.guild.configs.update("prefix", prefix, msg.guild);
+    return msg.send(`Changed Command Prefix for ${msg.guild.name} to ${prefix}`);
+  }
+};
