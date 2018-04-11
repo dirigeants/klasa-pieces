78cd85652cbdcc1bae73ae13628ee400f229f94f
Dwigoric <30539952+Dwigoric@users.noreply.github.com>
2018-04-11 23:08:45 +0800

    A fix for some commands (#47)
    * Shortened a string
    
    * Update fml.js
    
    * Update compliment.js
    
    * Update fml.js

--- commands/Misc/fml.js
+++ commands/Misc/fml.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
 		const embed = new this.client.methods.Embed()
 			.setTitle(`Requested by ${msg.author.tag}`)
 			.setAuthor('FML Stories')
-			.setColor(msg.member.highestRole.color || 0)
+			.setColor(msg.member.displayColor)
 			.setTimestamp()
 			.setDescription(`_${article.childNodes[0].text}\n\n_`)
 			.addField('I agree, your life sucks', updoot.childNodes[0].text, true)
