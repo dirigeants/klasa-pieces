ea572c745090aa21751cbad280cb2f77069fc156
Hutch <tech_hutch@verizon.net>
2018-04-12 09:49:04 -0400

    There were zero-width spaces between some of the chars (#48)

--- commands/System/exec.js
+++ commands/System/exec.js
@@ -15,8 +15,8 @@ module.exports = class extends Command {
 		const result = await this.client.methods.util.exec(code, { timeout: 30000 })
 			.catch(error => ({ stdout: null, stderr: error && error.message ? error.message : error }));
 
-		const output = result.stdout ? `**\`OUTPUT\`**${'`​`​`​prolog'}\n${result.stdout}\n${'`​`​`​'}` : '';
-		const outerr = result.stderr ? `**\`ERROR\`**${'`​`​`​prolog'}\n${result.stderr}\n${'`​`​`​'}` : '';
+		const output = result.stdout ? `**\`OUTPUT\`**${'```prolog'}\n${result.stdout}\n${'```'}` : '';
+		const outerr = result.stderr ? `**\`ERROR\`**${'```prolog'}\n${result.stderr}\n${'```'}` : '';
 
 		return msg.send([output, outerr].join('\n'));
 	}
