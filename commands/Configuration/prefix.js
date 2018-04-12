ddc9785e8b4c47517c64680eb5e2b79810dd6244
TheFloppyBanana <>
2018-04-12 14:30:10 +0800

    Add reset subcommand to revert guild prefix to default -TheFloppyBanana

--- commands/Configuration/prefix.js
+++ commands/Configuration/prefix.js
@@ -8,14 +8,20 @@ module.exports = class extends Command {
 			runIn: ['text'],
 			cooldown: 5,
 			description: 'Change the command prefix the bot uses in your server.',
-			usage: '[prefix:str{1,4}]'
+			usage: '[reset] [prefix:str{1,4}]',
+			subcommands: true
 		});
 	}
 
 	async run(msg, [prefix]) {
 		if (!prefix) return msg.send(`The current prefix for your guild is: \`${msg.guild.configs.prefix}\``);
 		await msg.guild.configs.update('prefix', prefix, msg.guild);
-		return msg.send(`Changed Command Prefix for ${msg.guild.name} to ${prefix}`);
+		return msg.send(`<:checkmark:415894323436191755>  ::  Changed Command Prefix for **${msg.guild.name}** to \`${prefix}\``);
+	}
+
+	async reset(msg) {
+		await msg.guild.configs.update('prefix', this.client.options.prefix, msg.guild);
+		return msg.send(`<:checkmark:415894323436191755>  ::  Switched back the guild\'s prefix back to \`${this.client.options.prefix}\`!`);
 	}
 
 };
