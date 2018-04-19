91f3443391f94a4c2870707695ed1d5f4208ea29
TheFloppyBanana <>
2018-04-19 13:15:44 +0800

    Update prefix.js

--- commands/Configuration/prefix.js
+++ commands/Configuration/prefix.js
@@ -4,24 +4,24 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			permLevel: 6,
-			runIn: ['text'],
+			aliases: ['setPrefix'],
 			cooldown: 5,
 			description: 'Change the command prefix the bot uses in your server.',
-			usage: '[reset] [prefix:str{1,4}]',
-			subcommands: true
+			permLevel: 6,
+			runIn: ['text'],
+			usage: '[prefix:str{1,10}]'
 		});
 	}
 
 	async run(msg, [prefix]) {
-		if (!prefix) return msg.send(`The current prefix for your guild is: \`${msg.guild.configs.prefix}\``);
-		await msg.guild.configs.update('prefix', prefix, msg.guild);
-		return msg.send(`<:checkmark:415894323436191755>  ::  Changed Command Prefix for **${msg.guild.name}** to \`${prefix}\``);
+		if (msg.guild.configs.prefix === prefix) throw msg.language.get('CONFIGURATION_EQUALS');
+		await msg.guild.configs.update('prefix', prefix);
+		return msg.sendMessage(`The prefix for this guild has been set to ${prefix}`);
 	}
 
 	async reset(msg) {
-		await msg.guild.configs.update('prefix', this.client.options.prefix, msg.guild);
-		return msg.send(`<:checkmark:415894323436191755>  ::  Switched back the guild\'s prefix back to \`${this.client.options.prefix}\`!`);
+		await msg.guild.configs.update('prefix', this.client.options.prefix);
+		return msg.send(`Switched back the guild\'s prefix back to \`${this.client.options.prefix}\`!`);
 	}
 
 };
