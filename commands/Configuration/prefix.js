73731c848257813b70bd0f5437fa7bb57eb77794
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2018-04-20 07:08:37 +0800

    Added reset subcommand -TheFloppyBanana (#50)
    * Updated API in cat.js -TheFloppyBanana
    
    * Add reset subcommand to revert guild prefix to default -TheFloppyBanana
    
    * Update prefix.js
    
    * Update prefix.js
    
    * Fixed command
    
    * Update prefix.js

--- commands/Configuration/prefix.js
+++ commands/Configuration/prefix.js
@@ -9,14 +9,20 @@ module.exports = class extends Command {
 			description: 'Change the command prefix the bot uses in your server.',
 			permLevel: 6,
 			runIn: ['text'],
-			usage: '[prefix:str{1,10}]'
+			usage: '[reset|prefix:str{1,10}]'
 		});
 	}
 
 	async run(msg, [prefix]) {
+		if (prefix === 'prefix') return this.reset(msg);
 		if (msg.guild.configs.prefix === prefix) throw msg.language.get('CONFIGURATION_EQUALS');
 		await msg.guild.configs.update('prefix', prefix);
 		return msg.sendMessage(`The prefix for this guild has been set to ${prefix}`);
+	}
+
+	async reset(msg) {
+		await msg.guild.configs.update('prefix', this.client.options.prefix);
+		return msg.sendMessage(`Switched back the guild's prefix back to \`${this.client.options.prefix}\`!`);
 	}
 
 };
