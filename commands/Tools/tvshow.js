2a9f219b47a3f06997f8f1f74eaa32220c4f92be
Dwigoric <30539952+Dwigoric@users.noreply.github.com>
2018-04-30 14:48:12 +0800

    Update tvshow.js

--- commands/Tools/tvshow.js
+++ commands/Tools/tvshow.js
@@ -17,22 +17,22 @@ module.exports = class extends Command {
 
     async run(msg, [query, page = 1]) {
         const request = await snekfetch.get(`https://api.themoviedb.org/3/search/tv?api_key=${tmdbAPIkey}&query=${query}`);
+        const show = request.body.results[page - 1];
+        if (!show) throw `I couldn't find a TV show with title **${query}** in page ${page}.`;
 
-        const tmdb = request.body.results[page - 1];
-        if (!tmdb) throw `I couldn't find a TV show with title **${query}** in page ${page}.`;
         const embed = new this.client.methods.Embed()
             .setColor('RANDOM')
-            .setImage(`https://image.tmdb.org/t/p/original${tmdb.poster_path}`)
-            .setTitle(`${tmdb.name} (${page} out of ${request.body.results.length} results)`)
-            .setDescription(tmdb.overview)
-            .setFooter(`${this.client.user.username} uses the TMDb API but is not endorsed or certified by TMDb.`, 'https://www.themoviedb.org/static_cache/v4/logos/208x226-stacked-green-9484383bd9853615c113f020def5cbe27f6d08a84ff834f41371f223ebad4a3c.png');
-        if (tmdb.title !== tmdb.original_name) embed.addField('Original Title', tmdb.original_name, true);
+            .setImage(`https://image.tmdb.org/t/p/original${show.poster_path}`)
+            .setTitle(`${show.name} (${page} out of ${request.body.results.length} results)`)
+            .setDescription(show.overview)
+            .setFooter(`${this.client.user.username} uses the TMDb API but is not endorsed or certified by TMDb.`, 'https://www.themoviedb.org/static_cache/v4/logos/208x226-stacked-green-9484383bd9853615c113f020def5cbe27f6d08a84ff834f41371f223ebad4a3c.png'); // eslint-disable-line max-len
+        if (show.title !== show.original_name) embed.addField('Original Title', show.original_name, true);
         embed
-            .addField('Vote Count', tmdb.vote_count, true)
-            .addField('Vote Average', tmdb.vote_average, true)
-            .addField('Popularity', tmdb.popularity, true)
-            .addField('First Air Date', tmdb.first_air_date);
-            
+            .addField('Vote Count', show.vote_count, true)
+            .addField('Vote Average', show.vote_average, true)
+            .addField('Popularity', show.popularity, true)
+            .addField('First Air Date', show.first_air_date);
+
         return msg.send(embed);
     }
 
