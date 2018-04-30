b32e320a88a54b9af6ac7b7f92238022c75a19cc
Dwigoric <30539952+Dwigoric@users.noreply.github.com>
2018-04-30 14:39:06 +0800

    Update movie.js in accordance to klasa-lint

--- commands/Tools/movie.js
+++ commands/Tools/movie.js
@@ -17,23 +17,23 @@ module.exports = class extends Command {
 
     async run(msg, [query, page = 1]) {
         const request = await snekfetch.get(`https://api.themoviedb.org/3/search/movie?api_key=${tmdbAPIkey}&query=${query}`);
-        const tmdb = request.body.results[page - 1];
-        
-        if (!tmdb) throw `I couldn't find a movie with title **${query}** in page ${page}.`;
-        
+        const movie = request.body.results[page - 1];
+
+        if (!movie) throw `I couldn't find a movie with title **${query}** in page ${page}.`;
+
         const embed = new this.client.methods.Embed()
-            .setImage(`https://image.tmdb.org/t/p/original${tmdb.poster_path}`)
-            .setTitle(`${tmdb.title} (${page} out of ${request.body.results.length} results)`)
-            .setDescription(tmdb.overview)
-            .setFooter(`${this.client.user.username} uses the TMDb API but is not endorsed or certified by TMDb.`, 'https://www.themoviedb.org/static_cache/v4/logos/208x226-stacked-green-9484383bd9853615c113f020def5cbe27f6d08a84ff834f41371f223ebad4a3c.png');
-        if (tmdb.title !== tmdb.original_title) embed.addField('Original Title', tmdb.original_title, true);
+            .setImage(`https://image.tmdb.org/t/p/original${movie.poster_path}`)
+            .setTitle(`${movie.title} (${page} out of ${request.body.results.length} results)`)
+            .setDescription(movie.overview)
+            .setFooter(`${this.client.user.username} uses the TMDb API but is not endorsed or certified by TMDb.`, 'https://www.themoviedb.org/static_cache/v4/logos/208x226-stacked-green-9484383bd9853615c113f020def5cbe27f6d08a84ff834f41371f223ebad4a3c.png'); // eslint-disable-line max-len
+        if (movie.title !== movie.original_title) embed.addField('Original Title', movie.original_title, true);
         embed
-            .addField('Vote Count', tmdb.vote_count, true)
-            .addField('Vote Average', tmdb.vote_average, true)
-            .addField('Popularity', tmdb.popularity, true)
-            .addField('Adult Content', tmdb.adult ? 'Yep' : 'Nope', true)
-            .addField('Release Date', tmdb.release_date);
-            
+            .addField('Vote Count', movie.vote_count, true)
+            .addField('Vote Average', movie.vote_average, true)
+            .addField('Popularity', movie.popularity, true)
+            .addField('Adult Content', movie.adult ? 'Yep' : 'Nope', true)
+            .addField('Release Date', movie.release_date);
+
         return msg.send(embed);
     }
 
