2275394a88becfd06d29b8007e884bc1c548326d
gc <30398469+gach@users.noreply.github.com>
2018-06-01 23:59:03 +1000

    better error handling

--- commands/Tools/randomreddit.js
+++ commands/Tools/randomreddit.js
@@ -7,23 +7,25 @@ module.exports = class extends Command {
 		super(...args, {
 			cooldown: 2,
 			description: 'Returns a random reddit post on a given subreddit.',
-			usage: '<subreddit:str>',
-			usageDelim: ' '
+			usage: '<subreddit:str>'
 		});
+		this.errorMessage = `There was an error. Reddit may be down, or the subreddit doesnt exist.`;
 	}
 
-	async run(msg, [subredditName]) {
-		const random = await snekfetch
-			.get(`https://www.reddit.com/r/${subredditName}/random.json`)
-			.then(res => res.body);
+	async run(msg, [subreddit]) {
+		const { data } = await snekfetch
+			.get(`https://www.reddit.com/r/${subreddit}/random.json`)
+			.then(res => {
+				if (res.body.error) throw this.errorMessage;
+				return res.body[0].data.children[0];
+			})
+			.catch(() => { throw this.errorMessage; });
 
-		if (random.error) throw `That subreddit doesn't exist, or is banned/private.`;
-
-		if (random[0].data.children[0].data.over_18 && !msg.channel.nsfw) {
+		if (data.over_18 && !msg.channel.nsfw) {
 			throw 'I cant post a NSFW image in this channel unless you mark it as NSFW!';
 		}
 
-		return msg.sendMessage(random[0].data.children[0].data.url);
+		return msg.sendMessage(data.url);
 	}
 
 };
