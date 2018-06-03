fb549d22be8d5004f0dd921dd9d7c2b6a4072745
kyranet <kyradiscord@gmail.com>
2018-06-04 01:20:20 +0200

    RebirthDB provider, refactored RethinkDB provider

--- providers/rebirthdb.js
+++ providers/rebirthdb.js
@@ -1,14 +1,19 @@
 const { Provider, Type, util: { mergeDefault, isObject, makeObject } } = require('klasa');
-const rethink = require('rethinkdbdash');
+const { r } = require('rebirthdbts'); // eslint-disable-line id-length
 
 module.exports = class extends Provider {
 
 	constructor(...args) {
 		super(...args);
-		this.db = rethink(mergeDefault({
+		this.db = r;
+		this.pool = null;
+	}
+
+	async init() {
+		this.pool = await r.connectPool(mergeDefault({
 			db: 'test',
 			silent: false
-		}, this.client.options.providers.rethinkdb));
+		}, this.client.options.providers.rebirthdb));
 	}
 
 	get exec() {
@@ -22,63 +27,63 @@ module.exports = class extends Provider {
 
 	/* Table methods */
 
-	async hasTable(table) {
+	hasTable(table) {
 		return this.db.tableList().contains(table).run();
 	}
 
-	async createTable(table) {
+	createTable(table) {
 		return this.db.tableCreate(table).run();
 	}
 
-	async deleteTable(table) {
+	deleteTable(table) {
 		return this.db.tableDrop(table).run();
 	}
 
-	async sync(table) {
+	sync(table) {
 		return this.db.table(table).sync().run();
 	}
 
 	/* Document methods */
 
-	async getAll(table, entries = []) {
+	getAll(table, entries = []) {
 		if (entries.length) return this.db.table(table).getAll(...entries).run();
 		return this.db.table(table).run();
 	}
 
-	async getKeys(table, entries = []) {
+	getKeys(table, entries = []) {
 		if (entries.length) return this.db.table(table).getAll(...entries)('id').run();
 		return this.db.table(table).run();
 	}
 
-	async get(table, id) {
+	get(table, id) {
 		return this.db.table(table).get(id).run();
 	}
 
-	async has(table, id) {
+	has(table, id) {
 		return this.db.table(table).get(id).ne(null).run();
 	}
 
-	async getRandom(table) {
+	getRandom(table) {
 		return this.db.table(table).sample(1).run();
 	}
 
-	async create(table, id, value = {}) {
+	create(table, id, value = {}) {
 		return this.db.table(table).insert({ ...this.parseUpdateInput(value), id }).run();
 	}
 
-	async update(table, id, value = {}) {
+	update(table, id, value = {}) {
 		return this.db.table(table).get(id).update(this.parseUpdateInput(value)).run();
 	}
 
-	async replace(table, id, value = {}) {
+	replace(table, id, value = {}) {
 		return this.db.table(table).get(id).replace({ ...this.parseUpdateInput(value), id }).run();
 	}
 
-	async delete(table, id) {
+	delete(table, id) {
 		return this.db.table(table).get(id).delete();
 	}
 
-	async updateValue(table, path, value) {
+	updateValue(table, path, value) {
 		// { channels: { modlog: '340713281972862976' } } | undefined
 		if (isObject(path) && typeof value === 'undefined') {
 			return this.db.table(table).update(path).run();
@@ -89,10 +94,10 @@ module.exports = class extends Provider {
 			return this.db.table(table).update(makeObject(path, value)).run();
 		}
 
-		throw new TypeError(`Expected an object as first parameter or a string and a non-undefined value. Got: ${new Type(path)} and ${new Type(value)}`);
+		return Promise.reject(new TypeError(`Expected an object as first parameter or a string and a non-undefined value. Got: ${new Type(path)} and ${new Type(value)}`));
 	}
 
-	async removeValue(table, path) {
+	removeValue(table, path) {
 		// { channels: { modlog: true } }
 		if (isObject(path)) {
 			return this.db.table(table).replace(row => row.without(path)).run();
@@ -104,7 +109,7 @@ module.exports = class extends Provider {
 			return this.db.table(table).replace(row => row.without(rPath)).run();
 		}
 
-		throw new TypeError(`Expected an object or a string as first parameter. Got: ${new Type(path)}`);
+		return Promise.reject(new TypeError(`Expected an object or a string as first parameter. Got: ${new Type(path)}`));
 	}
 
 };
