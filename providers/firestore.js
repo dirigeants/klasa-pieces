cae90f3fd1224e19c9425e09027929326268d516
Ritvik Somayaji <dinnerb0n3@gmail.com>
2018-06-04 18:56:32 +0300

    Made it complaint with issue #328 (#71)
    * Made it complaint with issue #328
    
    * Made the requested changes
    
    * Made requested changes
    
    * Fixed eslint issues
    
    * Got rid of jsdoc, reorganized functions.
    
    * Removed the updateValue method.
    
    * Made requested changes.
    
    * Eslint Issues fixed.

--- providers/firestore.js
+++ providers/firestore.js
@@ -16,6 +16,7 @@
 
 const { Provider } = require('klasa');
 const firebase = require('firebase-admin');
+
 const { FieldValue } = firebase;
 
 module.exports = class extends Provider {
@@ -24,10 +25,6 @@ module.exports = class extends Provider {
 		super(...args);
 		this.db = null;
 	}
-	/**
-	 * Initiaizes the firestore
-	 * @private
-	 */
 	async init() {
 		await firebase.initializeApp({
 			credential: firebase.credential.cert(this.client.options.providers.firestore.credentials),
@@ -36,128 +33,53 @@ module.exports = class extends Provider {
 
 		this.db = firebase.firestore();
 	}
-	/**
-	 *Checks if the table/collection exists
-	 * @param {string} table The name of the table/collection to be checked for existance
-	 * @returns {Promise<boolean>}
-	 */
+
 	hasTable(table) {
-		return this.db.collection(table).get().then(col => !!col.size);
+		return this.db.collection(table).get().then(col => Boolean(col.size));
 	}
 
-	/**
-	 * This method is a dummy method, as new tables/collections are automatically created by firestore when needed.
-	 * @param {string} table Name of the collection to create.
-	 * @returns {Promise<Collection>}
-	 */
 	createTable(table) {
 		return this.db.collection(table);
 	}
 
-	/**
-	 * Returns all the documents of a collection
-	 * @param {string} table Name of the collection, for which all values is to be returned
-	 * @returns {Promise<Array>}
-	 */
-	getAll(table) {
-		return this.db.collection(table).get().then(snaps => snaps.docs.map(snap => snap.data()));
-	}
-	/**
-	 * Returns all the Keys/Docs of a collection/tanle.
-	 * @param {string} table Name of the collection/table for which all Keys/Docs is to be returned.
-	 * @returns {Promise<Array>}
-	 */
 	getKeys(table) {
 		return this.db.collection(table).get().then(snaps => snaps.docs.map(snap => snap.id));
 	}
-	/**
-	 * Returns a particular Document from the table/Collection.
-	 * @param {string} table Name of the collection/Table to get the value from
-	 * @param {string} id Name of the Document/Key to be returned
-	 * @returns {Promise<Any>}
-	 */
+
 	get(table, id) {
-		return this.db.collection(table).doc(id).get();
+		return this.db.collection(table).doc(id).get().then(snap => this.packData(snap.data(), snap.id));
 	}
 
-	/**
-	 * Checks if a collection has a particular Document/Key
-	 * @param {string} table Name of the collection/Table
-	 * @param {string} id Name of the Document/Key
-	 * @returns {Promise<boolean>}
-	 */
 	has(table, id) {
 		return this.db.collection(table).doc(id).get().then(data => data.exists);
 	}
 
-	/**
-	 * Creates a new Document/Field in a collection.
-	 * @param {string} table Name of the collection/table
-	 * @param {string} id Name of they Docment/Key
-	 * @param {(ConfigurationUpdateResultEntry[] | [string, any][] | Object<string, *>)} doc the object you want to insert in the table.
-	 * @returns {Promise<DocumentObject>}
-	 */
 	create(table, id, doc = {}) {
 		return this.db.collection(table).doc(id).set(this.parseUpdateInput(doc));
 	}
 
-	/**
-	 * Updates the values of a Document/Field
-	 * @param {string} table Name of the Collection that contains the Document
-	 * @param {string} id  Name of the Document/field
-	 * @param {(ConfigurationUpdateResultEntry[] | [string, any][] | Object<string, *>)} doc the object you want to insert in the table.
-	 * @returns {Promise<WriteResult>}
-	 */
 	update(table, id, doc) {
 		return this.db.collection(table).doc(id).update(this.parseUpdateInput(doc));
 	}
 
-	/**
-	 * Deleted a particular Document/Field from the Collection/Table
-	 * @param {string} table Name of the collection/table
-	 * @param {string} id Name of the Document/Key to delete
-	 * @returns {Promise<WriteResult>}
-	 */
-	async delete(table, id) {
-		return await this.db.collection(table).doc(id).delete();
+	delete(table, id) {
+		return this.db.collection(table).doc(id).delete();
 	}
-	/**
-	 * Replaces a Document with a new Document specified by the user *
-	 * @param {string} table Name of the Collection
-	 * @param {Object} id The Filter used to select the document to update
-	 * @param {Object} [doc={}] The Document that replaces the matching document
-	 * @returns {Promise}
-	 */
+
 	replace(...args) {
 		return this.create(...args);
 	}
 
-	/**
-	 * Update or insert a new value to all entries
-	 * @param {string} table Name of the Collectiom
-	 * @param {string} path The Field to be updated in al the Documents
-	 * @param {*} newValue The value to to updated in all the Documents
-	 */
-	async updateValue(table, path, newValue) {
-		const keys = await this.getKeys(table);
+	async getAll(table, filter = []) {
+		const data = await this.db.collection(table).get()
+			.then(snaps => snaps.docs.map(snap => this.packData(snap.data(), snap.id)));
 
-		if (typeof path === 'object' && typeof newValue === 'undefined') {
-			await Promise.all(keys.map(doc => this.update(table, doc, path)));
-		} else if (typeof path === 'string' && typeof newValue !== 'undefined') {
-			await Promise.all(keys.map(doc => this.update(table, doc, { path: newValue })));
-		} else {
-			throw new TypeError(`Expected an object as first parameter or a string and a non-undefined value. Got: ${typeof key} and ${typeof value}`);
-		}
+		return filter.length ? data.filter(nodes => filter.includes(nodes.id)) : data;
 	}
 
-	/**
-	 * Remove a value or object from all entries.
-	 * @param {string} table Name of the Collection
-	 * @param {string} path The Field to be updated in al the Documents
-	 * @param {*} newValue The value to to updated in all the Documents
-	 */
 	async removeValue(table, path) {
 		const keys = await this.getKeys(table);
+
 		if (typeof path === 'object' && typeof newValue === 'undefined') {
 			for (const key of Object.keys(path)) path[key] = FieldValue.deleteValue();
 			await Promise.all(keys.map(doc => this.update(table, doc, path)));
@@ -166,6 +88,13 @@ module.exports = class extends Provider {
 		} else {
 			throw new TypeError(`Expected an object as first parameter or a string and a non-undefined value. Got: ${typeof key} and ${typeof value}`);
 		}
+	}
+
+	packData(data, id) {
+		return {
+			...data,
+			id
+		};
 	}
 
 };
