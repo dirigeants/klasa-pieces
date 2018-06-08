70abaff155ebe9947349074f180f0b812bc10f9e
Kyra <kyranet@users.noreply.github.com>
2018-06-08 07:17:19 +0200

    Update heapsnapshot.js

--- commands/System/Admin/heapsnapshot.js
+++ commands/System/Admin/heapsnapshot.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
 				'Finally, open the profile and you will be given a table of all objects in your process, have fun!\n\nP.S:',
 				'heapsnapshot files are as big as the amount of RAM you use, in big bots, the snapshots can freeze the bot',
 				'much longer and the files can be much heavier.'
-			]
+			].join(' ')
 		});
 	}
 
