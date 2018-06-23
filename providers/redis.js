977f38aae97679aa5887347f54809b2fec0cef0c
Paranjpay <soumil.paranjpay@gmail.com>
2018-06-23 22:50:55 +0530

    add redis provider

---
+++ providers/redis.js
@@ -0,0 +1,83 @@
+const { Provider } = require('klasa');
+const { Client } = require('redis-nextra');
+
+module.exports = class extends Provider {
+
+	constructor(...args) {
+		super(...args, { enabled: true });
+
+		const { hosts, options } = this.client.providers.redis;
+		this.db = new Client(hosts, options);
+
+		this.db.on('ready', () => this.client.emit('debug', 'Redis initialized.'))
+			.on('serverReconnect', server => this.client.emit('warn', `Redis server: ${server.host.string} is reconnecting`))
+			.on('error', err => this.client.emit('err', err));
+	}
+
+	async init() {
+		/*
+         * You can optionally define this method which will be run when the bot starts
+         * (after login, so discord data is available via this.client)
+         */
+	}
+
+	shutdown() {
+		/*
+         * You can optionally define this method which will be run when the provider is
+         * removed from the ProviderStore.
+         */
+	}
+
+	hasTable(table) {
+		return this.db.tables.has(table);
+	}
+
+	createTable(table) {
+		return this.db.createTable(table);
+	}
+
+	deleteTable(table) {
+		return this.db.deleteTable(table);
+	}
+
+	getAll(table) {
+		return this.db.table(table).valuesJson('*');
+	}
+
+	get(table, id) {
+		return this.db.table(table).getJson(id);
+	}
+
+	has(table, id) {
+		return this.db.table(table).has(id);
+	}
+
+	getRandom(table) {
+		return this.db.table(table).srandmember();
+	}
+
+	create(table, id, data) {
+		return this.db.table(table).setJson(id, data);
+	}
+
+	set(...args) {
+		return this.create(...args);
+	}
+
+	insert(...args) {
+		return this.create(...args);
+	}
+
+	update(...args) {
+		return this.set(...args);
+	}
+
+	replace(...args) {
+		return this.set(...args);
+	}
+
+	delete(table, id) {
+		return this.db.table(table).del(id);
+	}
+
+};
