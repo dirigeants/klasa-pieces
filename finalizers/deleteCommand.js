67ca869d88b5e7676ec627b0e9b02c924457495d
Kyra <kyranet@users.noreply.github.com>
2018-06-28 23:21:31 +0200

    Update deleteCommand.js

--- finalizers/deleteCommand.js
+++ finalizers/deleteCommand.js
@@ -3,8 +3,7 @@ const { Finalizer } = require('klasa');
 module.exports = class extends Finalizer {
 
 	async run(msg) {
-		if (!msg.deletable && !msg.guild && !msg.guild.configs.deleteCommand) return null;
-		return msg.delete();
+		if (msg.guild && msg.guild.configs.deleteCommand && msg.deletable) await msg.delete();
 	}
 
 	async init() {
