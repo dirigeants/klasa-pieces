6c73047dde2a66ffa0361e49d61cd4f3ec3f49ba
Kyra <kyranet@users.noreply.github.com>
2018-07-25 14:33:53 +0200

    [WIP] Added Lord Of Helps (#80)
    * [WIP] Added Lord Of Evals
    
    * fix: Accessing to undefined
    
    * fix: Full handler handling
    
    * feat: added --all flag

---
+++ commands/General/Chat Bot Info/help.js
@@ -0,0 +1,106 @@
+const { Command, RichDisplay, util: { isFunction } } = require('klasa');
+const { MessageEmbed, Permissions } = require('discord.js');
+
+const PERMISSIONS_RICHDISPLAY = new Permissions([Permissions.FLAGS.MANAGE_MESSAGES, Permissions.FLAGS.ADD_REACTIONS]);
+const time = 1000 * 60 * 3;
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['commands', 'cmd', 'cmds'],
+			guarded: true,
+			description: (message) => message.language.get('COMMAND_HELP_DESCRIPTION'),
+			usage: '(Command:command)'
+		});
+
+		this.createCustomResolver('command', (arg, possible, message) => {
+			if (!arg || arg === '') return undefined;
+			return this.client.arguments.get('command').run(arg, possible, message);
+		});
+
+		// Cache the handlers
+		this.handlers = new Map();
+	}
+
+	async run(message, [command]) {
+		if (command) {
+			return message.sendMessage([
+				`= ${command.name} = `,
+				isFunction(command.description) ? command.description(message) : command.description,
+				message.language.get('COMMAND_HELP_USAGE', command.usage.fullUsage(message)),
+				message.language.get('COMMAND_HELP_EXTENDED'),
+				isFunction(command.extendedHelp) ? command.extendedHelp(message) : command.extendedHelp
+			], { code: 'asciidoc' });
+		}
+
+		if (!message.flags.all && message.guild && message.channel.permissionsFor(this.client.user).has(PERMISSIONS_RICHDISPLAY)) {
+			// Finish the previous handler
+			const previousHandler = this.handlers.get(message.author.id);
+			if (previousHandler) previousHandler.stop();
+
+			const handler = await (await this.buildDisplay(message)).run(await message.send('Loading Commands...'), {
+				filter: (reaction, user) => user.id === message.author.id,
+				time
+			});
+			handler.on('end', () => this.handlers.delete(message.author.id));
+			this.handlers.set(message.author.id, handler);
+			return handler;
+		}
+
+		const method = this.client.user.bot ? 'author' : 'channel';
+		return message[method].send(await this.buildHelp(message), { split: { char: '\n' } })
+			.then(() => { if (message.channel.type !== 'dm' && this.client.user.bot) message.sendMessage(message.language.get('COMMAND_HELP_DM')); })
+			.catch(() => { if (message.channel.type !== 'dm' && this.client.user.bot) message.sendMessage(message.language.get('COMMAND_HELP_NODM')); });
+	}
+
+	async buildHelp(message) {
+		const commands = await this._fetchCommands(message);
+		const { prefix } = message.guildConfigs;
+
+		const helpMessage = [];
+		for (const [category, list] of commands) {
+			helpMessage.push(`**${category} Commands**:\n`, list.map(this.formatCommand.bind(this, message, prefix, false)).join('\n'), '');
+		}
+
+		return helpMessage.join('\n');
+	}
+
+	async buildDisplay(message) {
+		const commands = await this._fetchCommands(message);
+		const { prefix } = message.guildConfigs;
+		const display = new RichDisplay();
+		const color = message.member.displayColor;
+		for (const [category, list] of commands) {
+			display.addPage(new MessageEmbed()
+				.setTitle(`${category} Commands`)
+				.setColor(color)
+				.setDescription(list.map(this.formatCommand.bind(this, message, prefix, true)).join('\n'))
+			);
+		}
+
+		return display;
+	}
+
+	formatCommand(message, prefix, richDisplay, command) {
+		const description = isFunction(command.description) ? command.description(message) : command.description;
+		return richDisplay ? `• ${prefix}${command.name} → ${description}` : `• **${prefix}${command.name}** → ${description}`;
+	}
+
+	async _fetchCommands(message) {
+		const run = this.client.inhibitors.run.bind(this.client.inhibitors, message);
+		const commands = new Map();
+		await Promise.all(this.client.commands.map((command) => run(command, true)
+			.then(() => {
+				const category = commands.get(command.category);
+				if (category) category.push(command);
+				else commands.set(command.category, [command]);
+			}).catch(() => {
+				// noop
+			})
+		));
+
+		return commands;
+	}
+
+};
