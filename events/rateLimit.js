f06a05b79d0e15487fb9e574a8d17cb2215747e5
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2018-09-10 23:06:19 +0800

    Add rateLimit event (#100)
    * Add rateLimit event
    
    * Change emitted event from 'wtf' to 'verbose'
    
    * Use the color class instead for displaying headers
    
    * Add a small note
    
    * Join array with newline
    
    * Minor change
    
    * Update rateLimit.js

---
+++ events/rateLimit.js
@@ -0,0 +1,21 @@
+/**
+ * KlasaConsoleConfig#useColor must be
+ * enabled in order to use this piece.
+ */
+const { Event, Colors } = require('klasa');
+
+const HEADER = new Colors({ text: 'red' }).format('[RATELIMIT]');
+
+module.exports = class extends Event {
+
+	run({ timeout, limit, method, route }) {
+		this.client.emit('verbose', [
+			HEADER,
+			`Timeout: ${timeout}ms`,
+			`Limit: ${limit} requests`,
+			`Method: ${method.toUpperCase()}`,
+			`Route: ${route}`
+		].join('\n'));
+	}
+
+};
