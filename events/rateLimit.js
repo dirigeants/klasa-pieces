92e346ca6d32758ea873d42792dca66a8541caa3
TheFloppyBanana <angelolanceseraspi@gmail.com>
2018-09-10 20:48:12 +0800

    Use the color class instead for displaying headers

--- events/rateLimit.js
+++ events/rateLimit.js
@@ -1,12 +1,17 @@
-const { Event } = require('klasa');
+const { Event, Colors } = require('klasa');
 
-const HEADER = `\x1b[31m[RATELIMIT]\x1b[0m`;
+const HEADER = new Colors({ text: 'red' });
 
 module.exports = class extends Event {
 
-	run({ timeout, limit, method, path, route }) {
-		this.client.emit('verbose',
-			`${HEADER} \nTimeout: ${timeout}ms\nLimit: ${limit} requests\nMethod: ${method.toUpperCase()}\nPath ${path}\nRoute: ${route}`);
+	run({ timeout, limit, method, route }) {
+		this.client.emit('verbose', [
+			`${HEADER.format('[RATELIMIT]')}`,
+			`Timeout: ${timeout}ms`,
+			`Limit: ${limit} requests`,
+			`Method: ${method.toUpperCase()}`,
+			`Route: ${route}`
+		]);
 	}
 
 };
