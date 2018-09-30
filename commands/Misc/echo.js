db9a0d17af190f0c8be6c93a6a94702805f2cec1
Frangu Vlad <kingdgrizzle@gmail.com>
2018-09-30 23:09:07 +0300

    Update echo.js

--- commands/Misc/echo.js
+++ commands/Misc/echo.js
@@ -7,7 +7,7 @@ module.exports = class extends Command {
 			permissionLevel: 6,
 			runIn: ['text'],
 			description: 'Send a message to a channel through the bot.',
-			usage: '[channel:channel] <message:string> [...]',
+			usage: '[channel:channel] <message:...string>',
 			usageDelim: ' '
 		});
 	}
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 	async run(msg, [channel = msg.channel, ...message]) {
 		if (channel.guild !== msg.guild) throw 'You can\'t echo in other servers!';
 		if (!channel.postable) throw 'The selected channel is not postable.';
-		return channel.send(message.join(' '));
+		return channel.send(message);
 	}
 
 };
