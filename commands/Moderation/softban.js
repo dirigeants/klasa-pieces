af4428349906ed67c751013485856c8333991c7e
Frangu Vlad <kingdgrizzle@gmail.com>
2018-10-01 09:23:40 +0300

    Update softban.js

--- commands/Moderation/softban.js
+++ commands/Moderation/softban.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(msg, [user, days = 1, ...reason]) {
+	async run(msg, [user, days = 1, reason]) {
 		if (user.id === msg.author.id) throw 'Why would you ban yourself?';
 		if (user.id === this.client.user.id) throw 'Have I done something wrong?';
 
