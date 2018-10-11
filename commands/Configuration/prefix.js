a157229f4ceadeb0b039d9ed213adeb586432daa
York <aarghgames@gmail.com>
2018-10-11 10:34:53 +0100

    Update to prefix
    This will prevent the accidental erasing of the prefix.

--- commands/Configuration/prefix.js
+++ commands/Configuration/prefix.js
@@ -13,16 +13,17 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(msg, [prefix]) {
-		if (prefix === 'reset') return this.reset(msg);
-		if (msg.guild.settings.prefix === prefix) throw msg.language.get('CONFIGURATION_EQUALS');
-		await msg.guild.settings.update('prefix', prefix);
-		return msg.sendMessage(`The prefix for this guild has been set to ${prefix}`);
+	async run(message, [prefix]) {
+		if (!prefix) return message.send(`The prefix for this guild is \`${message.guild.settings.prefix}\``);
+		if (prefix === 'reset') return this.reset(message);
+		if (message.guild.settings.prefix === prefix) throw message.language.get('CONFIGURATION_EQUALS');
+		await message.guild.settings.update('prefix', prefix);
+		return message.send(`The prefix for this guild has been set to \`${prefix}\``);
 	}
 
-	async reset(msg) {
-		await msg.guild.settings.update('prefix', this.client.options.prefix);
-		return msg.sendMessage(`Switched back the guild's prefix back to \`${this.client.options.prefix}\`!`);
+	async reset(message) {
+		await message.guild.settings.update('prefix', this.client.options.prefix);
+		return message.send(`Switched back the guild's prefix back to \`${this.client.options.prefix}\`!`);
 	}
 
 };
