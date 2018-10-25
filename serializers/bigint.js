a570101c89cfc2184068409e6f19a757ba0187b8
Hutch <tech_hutch@verizon.net>
2018-10-25 09:56:10 -0400

    eslint

--- serializers/bigint.js
+++ serializers/bigint.js
@@ -1,3 +1,5 @@
+/* global BigInt:false */
+
 const { Serializer } = require('klasa');
 
 module.exports = class extends Serializer {
@@ -5,8 +7,9 @@ module.exports = class extends Serializer {
 	deserialize(data, piece, language) {
 		if (data instanceof BigInt) return data;
 		try {
+			// eslint-disable-next-line new-cap
 			return BigInt(data);
-		} catch (_) {
+		} catch (err) {
 			throw language.get('RESOLVER_INVALID_INT', piece.key);
 		}
 	}
