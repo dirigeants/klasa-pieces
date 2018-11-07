0acbeb0411abdd6adc0d449f3527338bce47e1bc
Garbage Collector <30398469+gc@users.noreply.github.com>
2018-11-08 05:05:54 +1100

    Add Spotify commands (#124)
    * Add Spotify command/task
    
    * add artist command
    
    * requested changes
    
    * Update artist.js
    
    * Update song.js
    
    * Update getSpotifyToken.js

---
+++ tasks/getSpotifyToken.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
+const { Task } = require('klasa');
+const fetch = require('node-fetch');
+
+// Login to https://developer.spotify.com/dashboard/applications
+// Create an app & get your clientID/secret, place them below
+// Run every 1 hour to refresh: this.client.schedule.create("getSpotifyToken", "0 * * * *");
+const clientID = '';
+const clientSecret = '';
+
+const authorization = Buffer.from(`${clientID}:${clientSecret}`).toString('base64');
+const options = {
+	method: 'POST',
+	body: 'grant_type=client_credentials',
+	headers: {
+		Authorization: `Basic ${authorization}`,
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+};
+
+module.exports = class extends Task {
+
+	async run() {
+		this._getToken();
+	}
+
+	async init() {
+		this._getToken();
+	}
+
+	async _getToken() {
+		try {
+			this.client._spotifyToken = await fetch(`https://accounts.spotify.com/api/token`, options)
+				.then(response => response.json())
+				.then(response => response.access_token);
+		} catch (error) {
+			this.client.emit('wtf', error);
+		}
+	}
+
+};
