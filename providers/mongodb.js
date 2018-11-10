f40c4054ae77a6aab76d668a22bcdf769b08d168
ndr3w221 <kalaz.bb@gmail.com>
2018-11-10 14:10:09 +0200

    Fixed mongo (#141)
    * Fix
    
    Fix
    
    * lint: Fixed lint and reduced line length

--- providers/mongodb.js
+++ providers/mongodb.js
@@ -16,13 +16,9 @@ module.exports = class extends Provider {
 			db: 'klasa',
 			options: {}
 		}, this.client.options.providers.mongodb);
-		const mongoClient = await Mongo.connect(`mongodb://${connection.host}:${connection.port}/`, mergeObjects(connection.options, {
-			auth: {
-				user: connection.user,
-				password: connection.password
-			},
-			useNewUrlParser: true
-		}));
+		const mongoClient = await Mongo.connect(
+			`mongodb://${connection.user}:${connection.password}@${connection.host}:${connection.port}/${connection.db}`,
+			mergeObjects(connection.options, { useNewUrlParser: true }));
 		this.db = mongoClient.db(connection.db);
 	}
 
