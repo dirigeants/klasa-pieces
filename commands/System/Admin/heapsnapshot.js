019e504501856a67b86728314dbb41757a32a22b
ndr3w221 <kalaz.bb@gmail.com>
2018-11-10 18:30:18 +0200

    Update heapsnapshot.js

--- commands/System/Admin/heapsnapshot.js
+++ commands/System/Admin/heapsnapshot.js
@@ -24,13 +24,13 @@ module.exports = class extends Command {
 	}
 
 	async run(msg) {
-		await msg.sendMessage('Capturing HEAP Snapshot. This may take a while...');
+		await msg.channel.send('Capturing HEAP Snapshot. This may take a while... HEAP Snapshot will be sent in your DMs!');
 
 		// Capture the snapshot (this freezes the entire VM)
 		const path = join(process.cwd(), `${Date.now()}.heapsnapshot`);
 		await writeSnapshot(path);
 
-		return msg.sendMessage(`Captured in \`${path}\`, check! Remember, do NOT share this with anybody, it may contain a lot of sensitive data.`);
+		return msg.author.send(`Captured in \`${path}\`, check! Remember, do NOT share this with anybody, it may contain a lot of sensitive data.`);
 	}
 
 };
