32f625a8327d097e9d8d547693adc2f59e4d9bb6
Hutch <tech_hutch@verizon.net>
2018-11-15 17:12:10 -0500

    changed and simplified bigint display a lot

--- commands/System/Admin/reboot.js
+++ commands/System/Admin/reboot.js
@@ -1,5 +1,4 @@
 // Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
-/* global BigInt */
 
 /*
  * In your main file, put:
@@ -14,43 +13,30 @@
 
 const { Command } = require('klasa');
 
-const PRECISE_TIME = {
-	MICROSECOND: 1000,
-	MILLISECOND: 1000 ** 2,
-	SECOND: 1000 ** 3
-};
+const DIGITS_TO_UNITS = new Map([
+	[9, 's'],
+	[6, 'ms'],
+	[3, 'μs']
+]);
 
 const rebootKeys = ['message', 'timestamp'].map(key => `restart.${key}`);
 
-function bigAbs(bigint) {
-	return bigint < 0 ? -bigint : bigint;
-}
+const bigAbs = bigint => bigint < 0 ? -bigint : bigint;
 
-function truncateWithRound(numString, digits) {
-	// eslint-disable-next-line eqeqeq
-	if (digits == null) return numString;
-	digits++;
-	const strWithExtraDigit = numString.substring(0, digits).padEnd(digits, '0');
-	const lastDigit = Number(strWithExtraDigit[strWithExtraDigit.length - 2]);
-	const strSansLastDigit = strWithExtraDigit.slice(0, -2);
-	return `${strSansLastDigit}${Number(strWithExtraDigit[strWithExtraDigit.length - 1]) >= 5 ? lastDigit + 1 : lastDigit}`;
-}
+const roundTwoDigits = ([first, second]) => `${Number(first) + (second >= 5)}${second}`;
 
-function bigDivideToString(bigint, divisor, digits) {
-	// eslint-disable-next-line new-cap
-	divisor = BigInt(divisor);
-	const intPart = bigint / divisor;
-	const decPart = bigint - (intPart * divisor);
-	return `${intPart}.${truncateWithRound(decPart.toString(), digits)}`;
-}
-
-function getFriendlyDuration(from, to = process.hrtime.bigint(), { digits = 2 } = {}) {
-	const time = to - from;
-	const absTime = bigAbs(time);
-	if (absTime >= PRECISE_TIME.SECOND) return `${bigDivideToString(time, PRECISE_TIME.SECOND, digits)}s`;
-	if (absTime >= PRECISE_TIME.MILLISECOND) return `${bigDivideToString(time, PRECISE_TIME.MILLISECOND, digits)}ms`;
-	if (absTime >= PRECISE_TIME.MICROSECOND) return `${bigDivideToString(time, PRECISE_TIME.MICROSECOND, digits)}μs`;
-	return `${time.toString()}ns`;
+function getFriendlyDuration(from, to = process.hrtime.bigint()) {
+	const time = bigAbs(to - from).toString();
+	const digits = time.length;
+	let shift, suffix;
+	for (const [d, suf] of DIGITS_TO_UNITS) {
+		if (digits > d) {
+			shift = -d;
+			suffix = suf;
+			break;
+		}
+	}
+	return `${time.slice(0, shift)}.${roundTwoDigits(time.slice(shift, shift + 2))}${suffix}`;
 }
 
 module.exports = class extends Command {
@@ -78,14 +64,14 @@ module.exports = class extends Command {
 
 	async init() {
 		// "message" needs to be awaited
-		const [message, timestamp] = await Promise.all(rebootKeys.map(key => this.resolveSetting(key)));
+		const [message, timestamp] = await Promise.all(rebootKeys.map(key => this._resolveSetting(key)));
 		await this.client.settings.reset(rebootKeys);
 
 		if (message) message.send(`✅ Successfully rebooted. (Took: ${timestamp && getFriendlyDuration(timestamp)})`);
 		else this.client.emit('info', 'No restart channel');
 	}
 
-	resolveSetting(path) {
+	_resolveSetting(path) {
 		const { settings, languages: { default: language } } = this.client;
 
 		const route = typeof path === 'string' ? path.split('.') : path;
