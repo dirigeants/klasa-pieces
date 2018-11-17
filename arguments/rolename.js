ace6ff4d04f067ef1a04e6beec8d6745eaaa1871
ndr3w221 <kalaz.bb@gmail.com>
2018-11-17 17:25:07 +0200

    Lint fix x2

--- arguments/rolename.js
+++ arguments/rolename.js
@@ -35,8 +35,8 @@ module.exports = class extends Argument {
 			case 0: throw `${possible.name} Must be a valid name, id or role mention`;
 			case 1: return querySearch[0];
 			default: 
-            	if (querySearch[0].name.toLowerCase() === arg.toLowerCase()) return querySearch[0]
-            	throw `Found multiple matches: \`${querySearch.map(role => role.name).join('`, `')}\``;
+            			if (querySearch[0].name.toLowerCase() === arg.toLowerCase()) return querySearch[0];
+            			throw `Found multiple matches: \`${querySearch.map(role => role.name).join('`, `')}\``;
 		}
 	}
 
