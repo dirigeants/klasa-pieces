75be203205a92a3ed0d057bf2235d703a44750cb
etc <trashbxg@gmail.com>
2018-11-20 15:29:21 +0100

    Update say.js

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -1,19 +1,20 @@
+// Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
 const { Command } = require("klasa");
 
-class Say extends Command {
+module.exports = class extends Command {
   constructor(...args) {
     super(...args, {
       description: "Make the bot say something",
+      name: "say",
       usage: "<message:string>",
+      disableEveryone: true,
       permissionLevel: 4,
       aliases: ["echo"]
     });
   }
 
   async run(msg, [args]) {
-    msg.delete().catch(O_o=>{}); 
+    msg.delete().catch() => {} 
     return msg.channel.send(args);
   }
-}
-
-module.exports = Say;
+};
