1e2661dd1897b4c8c3448f89342dc4eeb383cfd5
etc <trashbxg@gmail.com>
2018-11-20 03:51:50 +0100

    Say.js

---
+++ commands/Fun/say.js
@@ -0,0 +1,19 @@
+const { Command } = require("klasa");
+
+class Say extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Make the bot say something",
+      usage: "<message:string>",
+      permissionLevel: 4,
+      aliases: ["echo"]
+    });
+  }
+
+  async run(msg, [args]) {
+    msg.delete().catch(O_o=>{}); 
+    return msg.send(args);
+  }
+}
+
+module.exports = Say;
