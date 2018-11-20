57bed0051159b8249d1f0ddfb437e01f3be77c67
etc <trashbxg@gmail.com>
2018-11-20 18:34:27 +0100

    ESlint completed
    Completed eslint.

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -2,20 +2,23 @@
 const { Command } = require('klasa');
 
 module.exports = class extends Command {
-  constructor(...args) {
-    super(...args, {
-      description: 'Make the bot say something',
-      name: 'say',
-      usage: '<message:string>',
-      disableEveryone: true,
-      permissionLevel: 4,
-      aliases: ['echo'],
-      usageDelim: ' '
-    });
-  }
 
-  async run(msg, [args] ) {
-    msg.delete().catch(() => {});
-    return msg.channel.send(args);
-  }
+	constructor(...args) {
+		super(...args, {
+			description: 'Make the bot say something',
+			name: 'say',
+			usage: '<message:string>',
+			disableEveryone: true,
+			permissionLevel: 4,
+			aliases: ['echo'],
+			usageDelim: ' '
+		});
+	}
+
+	async run(msg, [args]) {
+		// eslint-disable-next-line no-empty-function
+		msg.delete().catch(() => {});
+		return msg.channel.send(args);
+	}
+
 };
