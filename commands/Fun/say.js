b6d1be67bed7b8234a4a75e57eea6bb977af7891
etc <trashbxg@gmail.com>
2018-11-20 18:18:37 +0100

    eslint update

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -1,15 +1,15 @@
 // Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
-const { Command } = require("klasa");
+const { Command } = require('klasa');
 
 module.exports = class extends Command {
   constructor(...args) {
     super(...args, {
-      description: "Make the bot say something",
-      name: "say",
-      usage: "<message:string>",
+      description: 'Make the bot say something',
+      name: 'say',
+      usage: '<message:string>',
       disableEveryone: true,
       permissionLevel: 4,
-      aliases: ["echo"],
+      aliases: ['echo'],
       usageDelim: ' '
     });
   }
