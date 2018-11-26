c77a2c955492bc36fa466523feb921bb5469039d
Hutch <tech_hutch@verizon.net>
2018-11-26 13:22:28 -0500

    Improved exec.js

--- commands/System/exec.js
+++ commands/System/exec.js
@@ -9,17 +9,20 @@ module.exports = class extends Command {
 			description: 'Execute commands in the terminal, use with EXTREME CAUTION.',
 			guarded: true,
 			permissionLevel: 10,
-			usage: '<expression:string>'
+			usage: '<expression:string>',
+			extendedHelp: 'Times out in 60 seconds by default. This can be changed with --timeout=TIME_IN_MILLISECONDS'
 		});
 	}
 
 	async run(msg, [input]) {
+		await msg.sendMessage('Executing your command...');
+
 		const result = await exec(input, { timeout: 'timeout' in msg.flags ? Number(msg.flags.timeout) : 60000 })
 			.catch(error => ({ stdout: null, stderr: error }));
 		const output = result.stdout ? `**\`OUTPUT\`**${codeBlock('prolog', result.stdout)}` : '';
 		const outerr = result.stderr ? `**\`ERROR\`**${codeBlock('prolog', result.stderr)}` : '';
 
-		return msg.sendMessage([output, outerr].join('\n'));
+		return msg.sendMessage([output, outerr].join('\n') || 'Done. There was no output to stdout or stderr.');
 	}
 
 };
