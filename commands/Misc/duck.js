0afdc640aa497321a25d89f75c4848eabe89f5c6
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-18 19:49:44 -0500

    Update duck.js (#167)
    Updated API url for ducks.

--- commands/Misc/duck.js
+++ commands/Misc/duck.js
@@ -7,12 +7,12 @@ module.exports = class extends Command {
 		super(...args, {
 			aliases: ['randomduck', 'ducc'],
 			description: 'Grabs a random duck image from random-d.uk.',
-			extendedHelp: 'This command grabs a random duck from "https://api.random-d.uk/random".'
+			extendedHelp: 'This command grabs a random duck from "https://random-d.uk/api/v1/random".'
 		});
 	}
 
 	async run(msg) {
-		const url = await fetch('https://api.random-d.uk/random')
+		const url = await fetch('https://random-d.uk/api/v1/random')
 			.then(response => response.json())
 			.then(body => body.url);
 		return msg.channel.sendFile(url);
