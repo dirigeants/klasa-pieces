22a655c1bfaf0290f13e06c5afb5bc3a4a669323
Nox <40806092+Noxillio@users.noreply.github.com>
2019-02-20 21:30:26 -0500

    Why does Travis keep bitching

--- arguments/partialuser.js
+++ arguments/partialuser.js
@@ -2,16 +2,16 @@ const { Argument } = require('klasa');
 
 module.exports = class extends Argument {
 
-	async run(arg, possible) {
+	async run(arg) {
 		const user = await this.resolveUser(arg, this.client.users);
 		if (user) return user;
 		throw `Please specify a valid user.`;
 	}
-	
+
 	resolveUser(text, users, caseSensitive = false, wholeWord = false) {
 		return users.get(text) || users.find(user => this.checkUser(text, user, caseSensitive, wholeWord));
 	}
-	
+
 	checkUser(text, user, caseSensitive = false, wholeWord = false) {
 		if (user.id === text) return true;
 		const reg = /<@!?(\d{17,19})>/;
