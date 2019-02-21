c47df24437730077f9606fd7aa99d7dde4f96123
Nox <40806092+Noxillio@users.noreply.github.com>
2019-02-20 19:32:43 -0500

    Create partialuser.js

---
+++ arguments/partialuser.js
@@ -0,0 +1,32 @@
+const { Argument } = require('klasa');
+
+module.exports = class extends Argument {
+
+	async run(arg, possible, message) {
+		const user = await this.resolveUser(arg, this.client.users);
+		if (user) return user;
+		// throw message.language.get('RESOLVER_INVALID_USER', possible.name);
+		throw `Please specify a valid user.`;
+	}
+
+	resolveUser(text, users, caseSensitive = false, wholeWord = false) {
+        return users.get(text) || users.find(user => this.checkUser(text, user, caseSensitive, wholeWord));
+	}
+	
+	checkUser(text, user, caseSensitive = false, wholeWord = false) {
+        if (user.id === text) return true;
+        const reg = /<@!?(\d{17,19})>/;
+        const match = text.match(reg);
+        if (match && user.id === match[1]) return true;
+        text = caseSensitive ? text : text.toLowerCase();
+        const username = caseSensitive ? user.username : user.username.toLowerCase();
+        const discrim = user.discriminator;
+        if (!wholeWord) {
+            return username.includes(text)
+            || (username.includes(text.split('#')[0]) && discrim.includes(text.split('#')[1]));
+        }
+        return username === text
+        || (username === text.split('#')[0] && discrim === text.split('#')[1]);
+    }
+
+};
