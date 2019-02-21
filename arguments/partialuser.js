e04081df291cb1c7f1801d2b3c7303b7ab8c0506
Nox <40806092+Noxillio@users.noreply.github.com>
2019-02-20 21:27:50 -0500

    Fix formatting for Travis CI

--- arguments/partialuser.js
+++ arguments/partialuser.js
@@ -2,30 +2,30 @@ const { Argument } = require('klasa');
 
 module.exports = class extends Argument {
 
-	async run(arg, possible, message) {
+	async run(arg, possible) {
 		const user = await this.resolveUser(arg, this.client.users);
 		if (user) return user;
 		throw `Please specify a valid user.`;
 	}
-
+	
 	resolveUser(text, users, caseSensitive = false, wholeWord = false) {
-        return users.get(text) || users.find(user => this.checkUser(text, user, caseSensitive, wholeWord));
+		return users.get(text) || users.find(user => this.checkUser(text, user, caseSensitive, wholeWord));
 	}
 	
 	checkUser(text, user, caseSensitive = false, wholeWord = false) {
-        if (user.id === text) return true;
-        const reg = /<@!?(\d{17,19})>/;
-        const match = text.match(reg);
-        if (match && user.id === match[1]) return true;
-        text = caseSensitive ? text : text.toLowerCase();
-        const username = caseSensitive ? user.username : user.username.toLowerCase();
-        const discrim = user.discriminator;
-        if (!wholeWord) {
-            return username.includes(text)
-            || (username.includes(text.split('#')[0]) && discrim.includes(text.split('#')[1]));
-        }
-        return username === text
-        || (username === text.split('#')[0] && discrim === text.split('#')[1]);
-    }
+		if (user.id === text) return true;
+		const reg = /<@!?(\d{17,19})>/;
+		const match = text.match(reg);
+		if (match && user.id === match[1]) return true;
+		text = caseSensitive ? text : text.toLowerCase();
+		const username = caseSensitive ? user.username : user.username.toLowerCase();
+		const discrim = user.discriminator;
+		if (!wholeWord) {
+			return username.includes(text) ||
+			(username.includes(text.split('#')[0]) && discrim.includes(text.split('#')[1]));
+		}
+		return username === text ||
+		(username === text.split('#')[0] && discrim === text.split('#')[1]);
+	}
 
 };
