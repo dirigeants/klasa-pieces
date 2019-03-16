800fd5776643f8b01e1c3ccc75b98ec7dc1a1372
Unknown <bdistin@gmail.com>
2019-03-16 10:03:35 -0500

    fix some things

--- src/tasks/jsonBackup.js
+++ src/tasks/jsonBackup.js
@@ -1,13 +1,13 @@
 // Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
 const { Task, Timestamp } = require('klasa');
-const { targz } = require('fs-nextra');
-const { resolve } = require('path');
+const { targz, ensureDir } = require('fs-nextra');
+const { resolve, dirname } = require('path');
 
 module.exports = class extends Task {
 
 	constructor(...args) {
 		super(...args);
-		this.timestamp = new Timestamp('yyyy-mm-ddThh-mm-ss');
+		this.timestamp = new Timestamp('YYYY-MM-DD HHmmss');
 	}
 
 	get provider() {
@@ -16,8 +16,10 @@ module.exports = class extends Task {
 
 	async run(data = { folder: './' }) {
 		if (!('folder' in data)) data = { folder: './' };
+		const file = resolve(data.folder, `json-backup-${this.timestamp}.tar.gz`);
 
-		await targz(resolve(data.folder, `json-backup-${this.timestamp}.tar.gz`), this.provider.baseDirectory);
+		await ensureDir(dirname(file));
+		await targz(file, this.provider.baseDirectory);
 	}
 
 };
