f5963ef8cec860470f6d9c63f8f2d3660a81b8c0
GC <30398469+gc@users.noreply.github.com>
2019-04-03 22:09:04 +1100

    Create adorableavatar.js

---
+++ commands/Fun/adorableavatar.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 dirigeants. All rights reserved. MIT license.
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Sends a avatar generated from adorable.io',
+			usage: '[name:str]'
+		});
+	}
+
+	async run(msg, [name]) {
+		return msg.send(`https://api.adorable.io/avatars/285/${
+			encodeURIComponent(name || msg.author.username || msg.id)
+		}.png`);
+	}
+
+};
