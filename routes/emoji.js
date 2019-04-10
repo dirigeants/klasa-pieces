b1352ea76f00d4881591735ac53b0dd7e7e0169c
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-10 16:54:18 -0400

    emoji and emojis route

---
+++ routes/emoji.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
+const { Route } = require('klasa-dashboard-hooks');
+
+module.exports = class extends Route {
+
+	constructor(...args) {
+		super(...args, { route: 'guilds/:guildID/emojis' });
+	}
+
+	get(request, response) {
+		const { guildID } = request.params;
+		const guild = this.client.guilds.get(guildID);
+		if (!guild) return response.end('[]');
+		return response.end(guild.emojis.keyArray());
+	}
+
+};
---
+++ routes/emojis.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
+const { Route } = require('klasa-dashboard-hooks');
+
+module.exports = class extends Route {
+
+	constructor(...args) {
+		super(...args, { route: 'guilds/:guildID/emojis/:emojiID' });
+	}
+
+	get(request, response) {
+		const { guildID, emojiID } = request.params;
+		const guild = this.client.guilds.get(guildID);
+		if (!guild) return response.end('{}');
+		const emoji = guild.emojis.get(emojiID);
+		if (!emoji) return response.end('{}');
+		return response.end(JSON.stringify(emoji));
+	}
+
+};
