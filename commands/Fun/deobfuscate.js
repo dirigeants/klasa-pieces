c6f4c29e33247b60c51b49b14a3673eddce6ef22
gc <30398469+gach@users.noreply.github.com>
2019-05-16 23:37:08 +1000

    add some pieces

---
+++ commands/Fun/deobfuscate.js
@@ -0,0 +1,17 @@
+const { Command } = require('klasa');
+const { remove } = require('confusables');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			usage: '<str:str{1,2000}>',
+			description: 'Deobfuscate a string which has confusable unicode characters.'
+		});
+	}
+
+	async run(msg, [str]) {
+		return msg.send(remove(str));
+	}
+
+};
---
+++ commands/Fun/obfuscate.js
@@ -0,0 +1,23 @@
+const { Command } = require('klasa');
+const { obfuscate } = require('confusables');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			usage: '<str:str{1,200}>',
+			description: 'Modify a string to have confusing letters in it.'
+		});
+	}
+
+	async run(msg, [str]) {
+		const variations = [];
+
+		for (let i = 0; i < 5; i++) {
+			variations.push(obfuscate(str));
+		}
+
+		return msg.send(variations.join('\n'));
+	}
+
+};
