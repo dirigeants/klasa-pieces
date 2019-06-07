349f7d421b8586c820dfdbb4473440362735331c
Hutch <tech_hutch@verizon.net>
2019-06-07 19:09:13 -0400

    thanks for checking this file before I commited it, eslint. asshole.

--- extendables/settingsResolve.js
+++ extendables/settingsResolve.js
@@ -4,23 +4,23 @@ const { Extendable, Settings, SchemaPiece } = require('klasa');
 
 module.exports = class extends Extendable {
 
-  constructor(...args) {
-    super(...args, { appliesTo: [Settings] });
-  }
+	constructor(...args) {
+		super(...args, { appliesTo: [Settings] });
+	}
 
-  resolve(path, guild, language = guild ? guild.language : this.client.languages.default) {
-    const route = typeof path === 'string' ? path.split('.') : path;
-    const piece = this.gateway.schema.get(route);
-    if (!(piece && piece instanceof SchemaPiece)) throw undefined;
+	resolve(path, guild, language = guild ? guild.language : this.client.languages.default) {
+		const route = typeof path === 'string' ? path.split('.') : path;
+		const piece = this.gateway.schema.get(route);
+		if (!(piece && piece instanceof SchemaPiece)) throw undefined;
 
-    let objOrData = this;
-    for (const key of route) objOrData = objOrData[key];
+		let objOrData = this; // eslint-disable-line consistent-this
+		for (const key of route) objOrData = objOrData[key];
 
-    try {
-      return piece.serializer.deserialize(objOrData, piece, language, guild);
-    } catch (_) {
-      return undefined;
-    }
-  }
+		try {
+			return piece.serializer.deserialize(objOrData, piece, language, guild);
+		} catch (err) {
+			return undefined;
+		}
+	}
 
 };
