296e31e28ab2aa0d1d340e0733c7bf1b2b4e62ac
Santosh Bhandari <bsantosh909@gmail.com>
2019-10-21 12:31:15 +0545

    Add: Random Fox Command

---
+++ commands/Misc/fox.js
@@ -0,0 +1,21 @@
+const { Command } = require('klasa');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['randomfox'],
+			description: 'Grabs a random fox image from fandomfox.ca',
+			extendedHelp: 'This command grabs a random fox from "https://randomfox.ca/floof/".'
+		});
+	}
+
+	async run(msg) {
+		const url = await fetch('https://randomfox.ca/floof/')
+			.then(response => response.json())
+			.then(body => body.image);
+		return msg.channel.sendFile(url);
+	}
+
+};
